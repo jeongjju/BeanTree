820897a5a4f5b98590b3d51c0bf127be7cd115d7
jeongju.noh <jeongju.noh@seculetter.com>
2019-10-28 15:28:39 +0900

    2019.10.28 폴더 구조 변경 및 솔루션 속성 변경

--- MindMapApp.h
+++
@@ -1,9 +0,0 @@
-//MindMapApp.h
-#ifndef  _MINDMAPAPP_H
-#define _MINDMAPAPP_H
-#include<afxwin.h>
-class MindMapApp :public CWinApp {
-public:
-	virtual BOOL InitInstance();
-};
-#endif // ! _MINDMAPAPP_H
